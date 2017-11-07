9b51cb23fbf77c734228d6a27377db5a7b32ad35
P.S.Li <p.s.li19980101@gmail.com>
2017-11-07 12:10:52 +0800

    Version2

--- app/src/main/java/com/fju/guess/guess/MainActivity.java
+++ app/src/main/java/com/fju/guess/guess/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txtNum = (TextView) findViewById(R.id.txt_num);
+        txtNum.setText(Integer.toString(sec_num));
     }
 
     public void push (View view) {
